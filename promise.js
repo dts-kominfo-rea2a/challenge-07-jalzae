e0b395cea54c62af57c1271a5c9a75c543bcbca9
jalzae <jalzae@gmail.com>
2022-06-09 21:21:15 +0700

    terjawab

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (string) => {
+
+  let data = await promiseTheaterIXX();
+  let data2 = await promiseTheaterVGC();
+
+  let result = data.filter((e) => e.hasil == string);
+  let result1 = data2.filter((e) => e.hasil == string);
+
+  return result.length + result1.length;
+};
 
 module.exports = {
   promiseOutput,
